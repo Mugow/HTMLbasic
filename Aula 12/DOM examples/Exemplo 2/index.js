0a7dd64ef97136040478317a430156ad011d7a30
Murilo Rocco Cordeiro <68757162+Mugow@users.noreply.github.com>
2024-05-02 20:58:03 -0300

    Add files via upload

---
+++ Aula 12/DOM examples/Exemplo 2/index.js
@@ -0,0 +1,5 @@
+document.querySelector("h1").style.color = "green";
+var buttons = document.querySelectorAll("button")
+buttons.forEach(function(button){
+    button.style.backgroundColor = "green";
+})
